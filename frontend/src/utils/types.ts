4dc6854c79dce9f32e20ece918c8ea3781e0fdbd
Alexander Safronenko <agsafronenko@gmail.com>
2025-03-14 16:45:53 +0100

    Add types for audioUtils

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,7 @@
+export interface AudioRefs {
+  themeAudio: HTMLAudioElement | null;
+  pauseAudio: HTMLAudioElement | null;
+  levelUpAudio: HTMLAudioElement | null;
+  rowClearAudio: HTMLAudioElement | null;
+  gameOverAudio: HTMLAudioElement | null;
+}
